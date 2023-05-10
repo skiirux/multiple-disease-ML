854415fb07bad228b72f8a6476dbd2af83e62340
skiirux <mohitkumar637@gmail.com>
2023-05-10 13:12:18 +0530

    removed some bugs

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 
 app = Flask(__name__)
 
+
 def predict(values, dic):
     # diabetes
     if len(values) == 8:
@@ -42,53 +43,59 @@ def predict(values, dic):
         dic.update(dic2)
         values2 = list(map(float, list(dic.values())))
 
-        model = pickle.load(open('models/diabetes.pkl','rb'))
+        model = pickle.load(open('models/diabetes.pkl', 'rb'))
         values = np.asarray(values2)
         return model.predict(values.reshape(1, -1))[0]
 
     # breast_cancer
     elif len(values) == 22:
-        model = pickle.load(open('models/breast_cancer.pkl','rb'))
+        model = pickle.load(open('models/breast_cancer.pkl', 'rb'))
         values = np.asarray(values)
         return model.predict(values.reshape(1, -1))[0]
 
     # heart disease
     elif len(values) == 13:
-        model = pickle.load(open('models/heart.pkl','rb'))
+        model = pickle.load(open('models/heart.pkl', 'rb'))
         values = np.asarray(values)
         return model.predict(values.reshape(1, -1))[0]
 
     # kidney disease
     elif len(values) == 24:
-        model = pickle.load(open('models/kidney.pkl','rb'))
+        model = pickle.load(open('models/kidney.pkl', 'rb'))
         values = np.asarray(values)
         return model.predict(values.reshape(1, -1))[0]
 
     # liver disease
     elif len(values) == 10:
-        model = pickle.load(open('models/liver.pkl','rb'))
+        model = pickle.load(open('models/liver.pkl', 'rb'))
         values = np.asarray(values)
         return model.predict(values.reshape(1, -1))[0]
+
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
+
 @app.route("/diabetes", methods=['GET', 'POST'])
 def diabetesPage():
     return render_template('diabetes.html')
+
 
 @app.route("/cancer", methods=['GET', 'POST'])
 def cancerPage():
     return render_template('breast_cancer.html')
 
+
 @app.route("/heart", methods=['GET', 'POST'])
 def heartPage():
     return render_template('heart.html')
 
+
 @app.route("/kidney", methods=['GET', 'POST'])
 def kidneyPage():
     return render_template('kidney.html')
+
 
 @app.route("/liver", methods=['GET', 'POST'])
 def liverPage():
@@ -102,7 +109,8 @@ def liverPage():
 # def pneumoniaPage():
 #     return render_template('pneumonia.html')
 
-@app.route("/predict", methods = ['POST', 'GET'])
+
+@app.route("/predict", methods=['POST', 'GET'])
 def predictPage():
     try:
         if request.method == 'POST':
@@ -122,43 +130,6 @@ def predictPage():
 
     return render_template('predict.html', pred=pred)
 
-# @app.route("/malariapredict", methods = ['POST', 'GET'])
-# def malariapredictPage():
-#     if request.method == 'POST':
-#         try:
-#             img = Image.open(request.files['image'])
-#             img.save("uploads/image.jpg")
-#             img_path = os.path.join(os.path.dirname(__file__), 'uploads/image.jpg')
-#             os.path.isfile(img_path)
-#             img = tf.keras.utils.load_img(img_path, target_size=(128, 128))
-#             img = tf.keras.utils.img_to_array(img)
-#             img = np.expand_dims(img, axis=0)
-
-#             model = tf.keras.models.load_model("models/malaria.h5")
-#             pred = np.argmax(model.predict(img))
-#         except:
-#             message = "Please upload an image"
-#             return render_template('malaria.html', message=message)
-#             return render_template('malaria_predict.html', pred=pred)
-
-# @app.route("/pneumoniapredict", methods = ['POST', 'GET'])
-# def pneumoniapredictPage():
-#     if request.method == 'POST':
-#         try:
-#             img = Image.open(request.files['image']).convert('L')
-#             img.save("uploads/image.jpg")
-#             img_path = os.path.join(os.path.dirname(__file__), 'uploads/image.jpg')
-#             os.path.isfile(img_path)
-#             img = tf.keras.utils.load_img(img_path, target_size=(128, 128))
-#             img = tf.keras.utils.img_to_array(img)
-#             img = np.expand_dims(img, axis=0)
-
-#             model = tf.keras.models.load_model("models/pneumonia.h5")
-#             pred = np.argmax(model.predict(img))
-#         except:
-#             message = "Please upload an image"
-#             return render_template('pneumonia.html', message=message)
-#     return render_template('pneumonia_predict.html', pred=pred)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
